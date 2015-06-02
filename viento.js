81c805478c5f04c8836dc6bff61fee4d8eee5abb
Austin Jackson <au5ton@icloud.com>
2015-06-02 01:39:02 -0500

    Added beforeDelay and afterDelay

--- viento.js
+++ viento.js
@@ -6,25 +6,50 @@ function Viento() {
 
 Viento.prototype.fire = function(t) {
 
+    if(t.animation.beforeDelay === undefined) {
+        t.animation.beforeDelay = 0;
+    }
+    if(t.animation.afterDelay === undefined) {
+        t.animation.afterDelay = 0;
+    }
+
     if(t.animation.type === "entrance") {
         t.element.addEventListener("animationstart",function(){
             t.element.style.visibility = "visible";
         });
     }
 
-    t.element.style.animationName = t.animation.name;
-    t.element.style.animationDuration = t.animation.duration;
-    
-    t.element.addEventListener("animationend",function(){
-        try {
-            if(t.animation.type === "exit") {
-                t.element.style.visibility = "hidden";
-            }
-            t.callback();
-        }
-        catch(err){
-        }
-    });
+
+    setTimeout(
+        function(){
+
+            t.element.style.animationName = t.animation.name;
+            t.element.style.animationDuration = t.animation.duration;
+
+            setTimeout(
+                function(){
+   
+                    t.element.addEventListener("animationend",function(){
+                        try {
+                            if(t.animation.type === "exit") {
+                                t.element.style.visibility = "hidden";
+                            }
+                            t.callback();
+                        }
+                        catch(err){
+                        }
+                    });
+                },
+                t.animation.afterDelay
+            );
+
+
+
+        },
+        t.animation.beforeDelay
+    );
+
+
 
 }
 
